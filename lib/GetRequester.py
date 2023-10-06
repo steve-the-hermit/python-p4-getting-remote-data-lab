c4a0670132352aa76d7060d5e9d3a55b5d7ade4a
steve-the-hermit <stevemacharia309@gmail.com>
2023-10-06 13:10:49 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 class GetRequester:
 
@@ -7,7 +6,9 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
